3f8eb5fefd171f41c028deb7cbce5cd0597e9305
googoocone1 <googoocone1@naver.com>
2025-03-19 16:00:11 +0900

    헤더 수정 swiper css global.css로 옮김

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,16 @@
+import Image from "next/image";
+
 const Header = () => {
-  return <div className="w-full h-[50px] bg-red-50">Header</div>;
+  return (
+    <div className="w-full h-[50px] bg-white flex items-center justify-center">
+      <Image
+        src="/logo.svg"
+        width={162}
+        height={32}
+        alt="법무법인 스탠다드 로고"
+      ></Image>
+    </div>
+  );
 };
 
 export default Header;
