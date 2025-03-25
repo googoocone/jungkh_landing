ccdcf4f13daaaa77fe8a420bda33c897fd6e2de3
googoocone1 <googoocone1@naver.com>
2025-03-25 13:30:28 +0900

    파비콘 등록

--- app/layout.js
+++ app/layout.js
@@ -14,6 +14,9 @@ const notoSansKR = Noto_Sans_KR({
 export const metadata = {
   title: "법무법인 스탠다드 정경현 대표변호사",
   description: "오직 법인회생 법인파산 사건만 처리한 전문 변호사입니다.",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({ children }) {
